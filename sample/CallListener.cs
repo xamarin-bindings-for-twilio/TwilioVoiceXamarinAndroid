e071e33a18999ac71b22282ef0b9021a600e3dbc
Bruno Coelho <4brunu@gmail.com>
2021-10-22 11:02:24 +0100

    Create sample project

---
+++ sample/CallListener.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Java.Interop;
+using TwilioVoice;
+
+namespace Sample
+{
+    public class CallListener : Java.Lang.Object, Call.IListener
+    {
+        #region Fields
+
+        private static CallListener _instance;
+
+        #endregion
+
+        #region Properties
+
+        public static CallListener Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new CallListener();
+                }
+
+                return _instance;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void OnCallQualityWarningsChanged(Call call, ICollection<Call.CallQualityWarning> currentWarnings, ICollection<Call.CallQualityWarning> previousWarnings)
+        {
+            Console.WriteLine("OnCallQualityWarningsChanged");
+        }
+
+        public void OnConnectFailure(Call p0, CallException p1)
+        {
+            Console.WriteLine("OnConnectFailure");
+        }
+
+        public void OnConnected(Call p0)
+        {
+            Console.WriteLine("OnConnected");
+        }
+
+        public void OnDisconnected(Call p0, CallException p1)
+        {
+            Console.WriteLine("OnDisconnected");
+        }
+
+        public void OnReconnected(Call p0)
+        {
+            Console.WriteLine("OnReconnected");
+        }
+
+        public void OnReconnecting(Call p0, CallException p1)
+        {
+            Console.WriteLine("OnReconnecting");
+        }
+
+        public void OnRinging(Call p0)
+        {
+            Console.WriteLine("OnRinging");
+        }
+
+        #endregion
+    }
+}
